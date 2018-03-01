901b43efb6d405f2646992769a828106cb79e863
pietroborrello <pietro.borrello95@gmail.com>
2018-03-01 14:29:43 +0100

    bug qemu detected

--- ropd/GadgetsCollector.py
+++ ropd/GadgetsCollector.py
@@ -169,6 +169,7 @@ def hook_err(uc, int_num, user_data):
         return True
     return False
 
+#TODO: qemu bug (Assertion failed: (map->sections_nb < TARGET_PAGE_SIZE), function phys_section_add_x86_64,file /private/tmp/pip-build-Awpvur/unicorn/src/qemu/exec.c, line 798)
 # callback for tracing invalid memory access (READ or WRITE)
 def hook_mem_invalid(uc, access, address, size, value, user_data):
     #memory access not necessarly aligned to page boundaries, so map two pages to be sure
